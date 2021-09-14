4ab38286753bde27ff56ad27afd36e5640ccb207
sskcallcenter <ssk4367858@gmail.com>
2021-09-15 03:24:12 +0900

    second commit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/test', function () {
+    return view('test');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
